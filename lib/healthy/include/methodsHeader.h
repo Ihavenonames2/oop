f9d39d202986b19a7bba30682502207ef7ca463e
NikitaS321 <nsidko123@gmail.com>
2022-10-19 02:54:25 +0300

    add_class_ex

--- lib/healthy/include/methodsHeader.h
+++ lib/healthy/include/methodsHeader.h
@@ -15,3 +15,45 @@ protected:
 	int weight;
 	int age;
 };
+class Exersices
+{
+public:
+	Exersices();
+	~Exersices();
+
+private:
+
+protected:
+	float weight;
+	float calories;
+};
+
+class Legs : public Exersices
+{
+public:
+	float Running();
+
+	float Squats();
+
+	float Jumps();
+
+};
+
+class Arms : public Exersices
+{
+public:
+	float Push_ups();
+
+	float Pull_ups();
+
+	float Dumbbell_exercises();
+
+};
+
+class Chest : public Exersices
+{
+public:
+	float Twisting();
+
+
+};
